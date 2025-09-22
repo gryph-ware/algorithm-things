d3e82b926011f3c8e3b7dbaf1e006ace0aa39fcf
gryph-ware <chanhnguyencute12@gmail.com>
2025-09-22 12:05:07 +0700

    hashmap/ongoing

---
+++ hashmap/hashmap.c
@@ -0,0 +1,97 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#ifdef _WIN32
+#define _CRT_SECURE_NO_WARNINGS
+#endif
+
+#define INITIAL_SIZE 97
+#define LOAD_FACTOR 0.85
+
+char *customDup(char *str){
+  if(str == NULL){
+    return NULL;
+  }
+  char *str_dup = malloc(strlen(str) + 1);
+  if(str != NULL){
+    strcpy(str_dup, str);
+  }
+  return str_dup;
+}
+
+/*=================define what hashmap must have==========*/
+typedef struct Node{
+  char *str_key;
+  int data;
+  int freq;
+  struct Node *next;
+} Node;
+
+typedef struct HashMap{
+  int size;
+  int capacity;
+  Node **table;
+}HashMap;
+
+/*===============create things=============*/
+HashMap *createMap(){
+  HashMap *map = malloc(sizeof(HashMap));
+  map->size = 0;
+  map->capacity = INITIAL_SIZE;
+  map->table = calloc(map->capacity, sizeof(Node*));
+  return map;
+}
+
+Node *createNode(char *str, int data){
+  Node *new = malloc(sizeof(Node));
+  new->str_key = customDup(str);
+  new->data = data;
+  new->freq = 1;
+  new->next = NULL;
+  return new;
+}
+
+/*===============hashmap's function=============*/
+unsigned long hash(char *str, int capacity){
+  unsigned long index = 5381;
+  int c;
+  while((c = *str++)){
+    index = ((index << 5) + index) + c;
+  }
+  return index % capacity;
+}
+
+void insertNode(char *str, int data){
+  unsigned long index = hash(str);
+  Node *entry = map->table[index];
+  while(entry){
+    if(strcmp(entry->str_key, str) == 0){
+      entry->data = data;
+      entry->freq ++;
+      return;
+    }
+    entry = entry->next;
+  }
+}
+
+void freeAll(HashMap *map){
+  for(int i = 0; i < map->capacity; i++){
+    Node *entry = map->table[i];
+    while(entry){
+      Node *temp = entry;
+      entry = entry->next;
+      free(temp->str_key);
+      free(temp);
+    }
+  }
+  free(map);
+}
+
+int main(){
+
+  HashMap *myMap = createMap();
+
+  freeAll(myMap);
+  return EXIT_SUCCESS;
+}
